9896e87acec3dbbf0d3f88cddb75f1136e01651d
chanmi12 <chanme1213@gmail.com>
2023-05-21 22:32:11 +0900

    create product.h

--- product.h
+++ product.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 typedef struct{
         char name[100];
         int price;
@@ -8,3 +8,8 @@ typedef struct{
         int discount;
 }Cart;
 
+void addProduct(Cart cart[], int *count); //상품 추가
+void updateProduct(Cart cart[], int count); //상품 수정
+void deleteProduct(Cart cart[], int *count); //상품 삭제
+void saveCartToFile(Cart cart[], int count, char *filename); //파일 저장
+void loadCartFromFile(Cart cart[], int *count, char *filename); //파일을  불러옴
